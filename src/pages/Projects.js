a0c2289842d741a92db786868af2a19e2ca2e142
RazBracha <razi.bracha@gmail.com>
2023-12-24 11:48:46 +0200

    to phones

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,36 +1,49 @@
+import React, { useState } from 'react';
 import './Projects.css';
 import Bar from './Bar';
 
-
 function Projects() {
+  const projects = [
+    { "id": "1", "name": "ShelterHelper", "description": '/pictures/ShelterHelper.jpg', "image": '/pictures/dog.jpg' },
+    { "id": "2", "name": "Online coding app", "description": '/pictures/OnlineCodingApp.jpg', "image": '/pictures/code.jpg' },
+    { "id": "3", "name": "Nutrition server", "description": '/pictures/MealsServer.jpg', "image": '/pictures/nutrition.jpg' },
+    { "id": "4", "name": "Checkers", "description": '/pictures/Checkers-des.jpg', "image": '/pictures/checkers.jpg' }
+  ];
 
-  const projects = [{ "id": "1", "name": "ShelterHelper", "description": '/pictures/ShelterHelper.jpg', "image": `/pictures/dog.jpg` },
-  { "id": "2", "name": "Online coding app", "description": '/pictures/OnlineCodingApp.jpg', "image": '/pictures/code.jpg' },
-  { "id": "3", "name": "Nutrition server", "description": '/pictures/MealsServer.jpg', "image": '/pictures/nutrition.jpg' },
-  { "id": "4", "name": "Checkers", "description": '/pictures/Checkers-des.jpg', "image": '/pictures/checkers.jpg' }];
+  const [flippedCard, setFlippedCard] = useState(null);
+
+  const handleCardClick = (index) => {
+    setFlippedCard((prevIndex) => (prevIndex === index ? null : index));
+  };
 
   return (
     <>
       <div className='barDiv'>
         <Bar />
         <div className='cards'>
-          {projects.map((project) => (
-            <div className="card" key={project.id}>
+          {projects.map((project, index) => (
+            <div
+              key={project.id}
+              className={`card ${flippedCard === index ? 'flipped' : ''}`}
+              onClick={() => handleCardClick(index)}
+            >
               <div className="card-inner">
                 <div className="front">
                   <h2>{project.name}</h2>
                   <img alt='' className='image' src={project.image} />
-
                 </div>
                 <div className="back">
                   <img alt='' className='image' src={project.description} />
                 </div>
               </div>
             </div>
-          ))}</div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
 export default Projects;
+
+
