35779805e9f347f1fd702ab4c9d794bc7a6483e5
Raz Bracha <razi.bracha@gmail.com>
2024-08-01 19:36:26 +0300

    open link in new window

--- src/comps/ProjectCard.js
+++ src/comps/ProjectCard.js
@@ -1,18 +1,18 @@
-import '../styles/ProjectCard.css'
+import "../styles/ProjectCard.css";
 const ProjectCard = ({ project }) => {
-    return (
-        <div className="project-card">
-            <img src={project.image} alt={project.name} />
-            <div className='project-card-text-container'>
-                <h3 className='project-name'>{project.name}</h3>
-                <p className='project-description'>{project.description}</p>
-                <p className='project-tech'>{project.technologies}</p>
-                <a className="project-link" href={project.github}>
-                view on GitHub ↗
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="project-card">
+      <img src={project.image} alt={project.name} />
+      <div className="project-card-text-container">
+        <h3 className="project-name">{project.name}</h3>
+        <p className="project-description">{project.description}</p>
+        <p className="project-tech">{project.technologies}</p>
+        <a className="project-link" href={project.github} target="_blank">
+          view on GitHub ↗
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
